ffd34f791b017d471a1e61ef9bb38507bce6b066
E. Hannah <echannah631@gmail.com>
2021-02-23 15:33:32 -0800

    created users view controller

---
+++ Flash Chat iOS13/Controllers/UsersViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UsersViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Elliot Hannah III on 2/23/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
