a27b80df8e70fa2ea9190e9e7c9bd3e0fcc2b4ca
E. Hannah <echannah631@gmail.com>
2021-02-23 16:06:11 -0800

    setup segues from register and login to users view controller

--- Flash Chat iOS13/Constants.swift
+++ Flash Chat iOS13/Constants.swift
@@ -10,8 +10,8 @@ struct K {
     static let appName = "⚡️FlashChat"
     static let cellIdentifier = "ReusableCell"
     static let cellNibName = "MessageCell"
-    static let registerSegue = "RegisterToChat"
-    static let loginSegue = "LoginToChat"
+    static let registerSegue = "RegisterToUsers"
+    static let loginSegue = "LoginToUsers"
     
     struct BrandColors {
         static let purple = "BrandPurple"
--- Flash Chat iOS13/Controllers/UsersViewController.swift
+++ Flash Chat iOS13/Controllers/UsersViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class UsersViewController: UIViewController {
+class AllUsersViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,14 +17,6 @@ class UsersViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
